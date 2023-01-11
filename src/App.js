164e6789d58af7bddf90499966ec0ef1b6b4b59d
Glenn Gravdal <gravdalgames@gmail.com>
2023-01-11 13:01:26 +0100

    Making components out of often used UI elements.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
+import Todo from "./components/todo";
+import Form from "./components/form";
+import FilterButton from "./components/filterButton";
 
-function App() {
+function App(props) {
+
+  const taskList = props.tasks?.map((task) => <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>Todotron</h1>
+      <Form />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+
+      </div>
+      <h2 id="list-heading">
+        3 tasks remaining
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
     </div>
   );
 }
