85810aa4e3bf7127a8328f5366bf3f448a311274
Glenn Gravdal <gravdalgames@gmail.com>
2023-01-12 14:36:27 +0100

    Concept done and accessible for screen readers.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 
 const DATA = [
-  { id: "todo-0", name: "Eat", completed: true},
+  { id: "todo-0", name: "Eat", completed: false},
   { id: "todo-1", name: "Sleep", completed: false},
   { id: "todo-2", name: "Repeat", completed: false},
-]
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
